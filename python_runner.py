2a6fcdc456c3ded756b3e499b7d347296d4b3a9f
Yabra <bagyar@yandex.ru>
2023-04-03 20:36:28 +0500

    Added python scripts checking module

--- python_runner.py
+++ python_runner.py
@@ -1,18 +1,56 @@
 from enum import Enum
-import traceback
+import subprocess
+import json
+import os
 
 
 class ReturnType(Enum):
     SUCCESS = 0
-    EXCEPTION = 1
+    WRONG = 1
+    EXCEPTION = 2
 
 
-def run_code(code: str) -> tuple[ReturnType.EXCEPTION, str]:
-    try:
-        exec(code)
-        return ReturnType.SUCCESS
-    except Exception as e:
-        return ReturnType.EXCEPTION, "Exception: " + e.__class__.__name__ + "\n" + ''.join(traceback.format_exception(etype=type(e), value=e, tb=e.__traceback__))
+def run_code(code: str, tests_json: str) -> tuple[ReturnType.EXCEPTION, str]:
+    code = "__input_file = open(\"input.txt\")\n" \
+           "def input(s=\"\"):\n" \
+           "    print(s, end=\"\")\n" \
+           "    return __input_file.readline().rstrip(\"\\n\")\n" \
+           "try:\n" + \
+           "\n".join(["    " + s for s in code.split("\n")]) + "\n" + \
+           "    __input_file.close()\n" \
+           "except Exception as e:\n" \
+           "    print(\"Exception: \" + e.__class__.__name__ + \"\\n\" +" \
+           " ''.join(traceback.format_exception(etype=type(e), value=e, tb=e.__traceback__)))\n"
+
+    file = open("./solution.py", "w")
+    file.write(code)
+    file.close()
+
+    tests = json.loads(tests_json)
+    for test in tests:
+        input_file = open("./input.txt", "w")
+        input_file.write(test[0])
+        input_file.close()
+
+        result = subprocess.check_output(['python3', './solution.py']).decode("utf-8")
+        if result != test[1]:
+            os.remove("./solution.py")
+            os.remove("./input.txt")
+            return ReturnType.WRONG
+
+        elif result.startswith("Exception:"):
+            return ReturnType.EXCEPTION, result
+
+    os.remove("./solution.py")
+    os.remove("./input.txt")
+    return ReturnType.SUCCESS
 
 
-print(run_code("print(\"Hello World\")\nEXCERTION!!!!!"))
+print(
+    run_code(
+        "s = input()\nprint(f\"Hello, {s}!\")",
+        json.dumps(
+            [("Ярослав", "Hello, Ярослав!\n"), ("Влад", "Hello, Влад!\n"), ("Pavel", "Hello, Pavel!\n")]
+        )
+    )
+)
